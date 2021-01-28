c8db8b904464d315e574f509fbf1ca85ccd3ded8
justin-mavity <justinmavity1021@gmail.com>
2021-01-28 13:08:37 -0600

    mvp

---
+++ friends/src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    headers: {
+      baseURL: "http://localhost:5000/api",
+      Authorization: token,
+    },
+  });
+};
